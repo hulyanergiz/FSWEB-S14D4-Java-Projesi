f16f33f10b52447a2c7b3c298536809331e92b84
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-18 12:32:49 +0300

    creates interfaces named as Bleedable and Poisonable

---
+++ src/com/workintech/game/Bleedable.java
@@ -0,0 +1,5 @@
+package com.workintech.game;
+
+public interface Bleedable {
+    double bleed();
+}
---
+++ src/com/workintech/game/Poisonable.java
@@ -0,0 +1,5 @@
+package com.workintech.game;
+
+public interface Poisonable {
+    double poison();
+}
