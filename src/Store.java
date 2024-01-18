770f0ee12ebf2d9c613d7b2a93749b248fecfefd
Hülya Nergiz <akin.hulya@outlook.com>
2024-01-18 12:31:43 +0300

    creates objects and gives them ProductForSale array as products and return their showDetails method messages

--- src/Store.java
+++ src/Store.java
@@ -1,10 +1,20 @@
+import com.workintech.products.Bread;
+import com.workintech.products.Chocolate;
+import com.workintech.products.Coke;
+import com.workintech.products.ProductForSale;
+
 public class Store {
     public static void main(String[] args) {
-
-        listProducts();
+        Chocolate chocolate=new Chocolate("bitter",15,"includes %60 cacao",60);
+        Coke coke=new Coke("zero",20,"no sugar",0);
+        Bread bread=new Bread("whole wheat",10,"whole wheated bread",200);
+        ProductForSale[] products=new ProductForSale[]{chocolate,coke,bread};
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+for(ProductForSale product:products){
+    product.showDetails();
+}
     }
 }
